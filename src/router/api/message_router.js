b90221ce92822bf0e838537c177aaaf2fc69dac6
Maria Lazar <maria.daria.lazar@gmail.com>
2020-08-25 22:54:57 +0300

    notif-partial

--- src/router/api/message_router.js
+++ src/router/api/message_router.js
@@ -14,7 +14,23 @@ messageRouter.get('/', checkAuth, (req, res) => {
             }
         });
     } else if (req.query.status) {
-        console.log("get all conversations with unread messages");
+        Message.getMessagesInfoOfUser(req.session.userId, (err, messages) => {
+            if (err) {
+                res.send(err);
+            } else {
+                let recentUsers = {};
+                messages.forEach(message => {
+                    if (req.session.userId == message.user_id2) {
+                        let userId = message.user_id1;
+                        if (!(userId in recentUsers) && message.status === req.query.status) {
+                            recentUsers[userId] = {name: message.name, date: message.deliveredTime};
+                        }
+                    }
+                });
+                recentUsers = Object.entries(recentUsers);
+                res.send(recentUsers);
+            }
+        });
     } else {
         let users = req.query.user;
         if (users.length === 2) {
@@ -25,48 +41,63 @@ messageRouter.get('/', checkAuth, (req, res) => {
                     res.send(messages);
                 }
             });
-        } else {
-            Message.getMessagesInfoOfUser(users, (err, messages) => {
-                if (err) {
-                    res.send(err);
-                } else {
-                    let recentUsers = {};
-                    messages.forEach(message => {
-                        let userId = message.user_id1;
-                        if (userId == req.session.userId) {
-                            userId = message.user_id2;
-                        }
-                        if (!(userId in recentUsers)) {
-                            recentUsers[userId] = {name: message.name, date: message.deliveredTime};
-                        }
-                    });
-                    recentUsers = Object.entries(recentUsers);
-                    recentUsers.sort((a, b) => {
-                        if (a[1]["date"] < b[1]["date"]) {
-                            return 1;
-                        } else if (a[1]["date"] > b[1]["date"]) {
-                            return -1;
-                        } else {
-                            return 0;
-                        }
-                    });
-                    res.send(recentUsers);
-                }
-            });
         }
     }
 });
 
-messageRouter.post('/', checkAuth, (req, res) => {
-    const newMessage = new Message(req.body);
-
-    Message.create(newMessage, function (err, message) {
+messageRouter.get('/history', checkAuth, (req, res) => {
+    Message.getMessagesInfoOfUser(req.session.userId, (err, messages) => {
         if (err) {
             res.send(err);
         } else {
-            res.json({error: false, message: "Message added successfully!", data: message});
+            let recentUsers = {};
+            messages.forEach(message => {
+                let userId = message.user_id1;
+                if (userId == req.session.userId) {
+                    userId = message.user_id2;
+                }
+                if (!(userId in recentUsers)) {
+                    recentUsers[userId] = {name: message.name, date: message.deliveredTime};
+                }
+            });
+            recentUsers = Object.entries(recentUsers);
+            recentUsers.sort((a, b) => {
+                if (a[1]["date"] < b[1]["date"]) {
+                    return 1;
+                } else if (a[1]["date"] > b[1]["date"]) {
+                    return -1;
+                } else {
+                    return 0;
+                }
+            });
+            res.send(recentUsers);
         }
     });
+});
+
+messageRouter.post('/', checkAuth, (req, res) => {
+    if (!req.query.user) {
+        const newMessage = new Message(req.body);
+
+        Message.create(newMessage, function (err, message) {
+            if (err) {
+                res.send(err);
+            } else {
+                res.json({error: false, message: "Message added successfully!", data: message});
+            }
+        });
+    } else {
+        let users = req.query.user;
+        if (users.length === 2) {
+            Message.setMessagesBetweenUsersRead(users, (err, messages) => {
+                if (err) {
+                    res.send(err);
+                } else {
+                    res.send(messages);
+                }
+            });
+        }
+    }
 });
 
 messageRouter.get('/:id', checkAuth, (req, res) => {
@@ -78,5 +109,6 @@ messageRouter.get('/:id', checkAuth, (req, res) => {
         }
     });
 });
+
 
 
