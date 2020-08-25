1dd96b0c2bb24ce1ac3a0fd93d4b97df4b95cf44
elena-gabriela-bogos <61230684+elena-gabriela-bogos@users.noreply.github.com>
2020-08-25 12:43:21 +0300

    search

--- config/db.config.js
+++ config/db.config.js
@@ -4,7 +4,8 @@ export const dbConn = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: 'password',
-    database: 'codingne_meditations'
+    database: 'codingne_meditations',
+    multipleStatements: true
 });
 
 dbConn.connect(function (err) {
