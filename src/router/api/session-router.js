937535e1a394a1313dfc2ea2f17b628fba4a76d9
silviasuciu <62290505+silviasuciu@users.noreply.github.com>
2020-08-26 11:55:49 +0300

    ,

--- src/router/api/session-router.js
+++ src/router/api/session-router.js
@@ -9,47 +9,20 @@ import {loginRouter} from "../login-router";
 import path from "path";
 
 export const sessionRouter = express.Router()
-var id = 3;
 
 sessionRouter.get('/', (req, res) => {
-    { res.render(path.resolve('public/views/menteeSessionHistory.ejs'),);
+    res.render(path.resolve('public/views/menteeSessionHistory.ejs'));
 
-        let mentees = req.query.mentee; //skills
-        console.log(req.mentee);
-        if (!(req.query.mentee instanceof Array)) {
-            mentees = [mentees];
-        }
-
-        Session.findByMentee(mentees[0], (err, sessions) => {
-            if (err) {
-                res.send(err);
-            } else {
-                res.send(sessions);
-            }
-        });
-    }
 });
+
+
 
 sessionRouter.post('/', (req, res) => {
+    console.log(req.body);
     const session = new Session(req.body);
-
-    Session.create(session, function (err, lf) {
-        if (err) {
-            res.send(err);
-        } else {
-            res.json({error: false, message: "Session added successfully!", data: lf});
-        }
-    });
-});
-
-mentorRouter.get('/:id', (req, res) => {
-    Session.findById(req.params.id, function (err, mentor) {
-        if (err) {
-            res.send(err);
-        } else {
-            res.json(mentor);
-        }
-    });
+    Session.findByMentee(req.body.id);
+    res.render(path.resolve('public/views/menteeSessionHistory.ejs'));
 
 
 });
+
