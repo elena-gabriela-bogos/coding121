2a55369561c2572e9642da52fcaf372a7d70720f
Maria Lazar <maria.daria.lazar@gmail.com>
2020-08-26 19:28:29 +0300

    mentor-chat

--- src/router/mentor/mentor-dashboard-router.js
+++ src/router/mentor/mentor-dashboard-router.js
@@ -1,17 +1,21 @@
-
 import express from 'express';
 import path from 'path';
 import User from "../../domain/user";
+import {checkAuth} from "../api/authentification";
 
 export const mentorDashboardRouter = express.Router()
 
-mentorDashboardRouter.get('/', (req, res) => {
-    if (req.session.loggedin) {
-        User.findById(req.session.userId, (err, user) => {
-            res.render(path.resolve('public/views/dashboardMentor.ejs'), {name: user[0].name, picture: user[0].picture});
-        });
-    } else {
-        res.redirect('/login');
-    }
+mentorDashboardRouter.get('/', checkAuth, (req, res) => {
+    User.findById(req.session.userId, (err, user) => {
+        res.render(path.resolve('public/views/dashboardMentor.ejs'),
+            {
+                name: user[0].name,
+                picture: user[0].picture,
+                id: user[0].id,
+                chatOpen: req.session.chatOpen,
+                chatPartner: req.session.chattingWith,
+                chatHistoryOpen: req.session.chatHistoryOpen
+            });
+    });
 });
 
