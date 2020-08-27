36b101fae96d1251555bc11cbd8d3953173b02d2
Maria Lazar <maria.daria.lazar@gmail.com>
2020-08-27 18:32:19 +0300

    session-start-popup

--- src/router/chat.js
+++ src/router/chat.js
@@ -1,4 +1,6 @@
 import Message from "../domain/message";
+import {v4 as uuidv4} from 'uuid';
+
 
 export const bindSocketChatEvents = (socket, io) => {
     socket.on('new_message', (data) => {
@@ -35,16 +37,29 @@ export const bindSocketChatEvents = (socket, io) => {
 
     socket.on('start-session-request', (data) => {
         if (data.to in io.sockets.adapter.rooms) {
-            io.to(data.to).emit("start-session-request", {from: data.from, fromName: data.fromName});
+            const roomId = uuidv4();
+            socket.join(roomId);
+            io.to(data.to).emit("start-session-request", {from: data.from, fromName: data.fromName, room: roomId});
         } else {
             io.to(data.from).emit("offline-user");
         }
     });
     socket.on('start-session-confirm', (data) => {
-        io.to(data.to).emit("start-session-confirm");
+        socket.handshake.session.busy = true;
+        socket.join(data.room);
+        // console.log(io.sockets.adapter.rooms[data.room].sockets);
+        io.to(data.to).emit("start-session-confirm", {room: data.room});
     });
 
     socket.on('start-session-refused', (data) => {
         io.to(data.to).emit("start-session-refused");
+    });
+
+    socket.on('session-request-cancelled', (data) => {
+        io.to(data.to).emit("cancel-session-request");
+    });
+
+    socket.on("user-busy", (data) => {
+        io.to(data.to).emit("user-busy");
     });
 }
